89e350631c1dfb791fcf4d849ed6a5b9d83b96e0
YohanSamitha <yohansamitha123@gmail.com>
2021-06-18 17:30:40 +0530

    added login page and inner components

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,17 @@ import {CommonModule} from '@angular/common';
 
 import {AuthRoutingModule} from './auth-routing.module';
 import {AuthComponent} from './auth.component';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {LoginItemsPageComponent} from './components/login-page/inner-items/login-items-page/login-items-page.component';
+import {SignUpItemsPageComponent} from './components/login-page/inner-items/sign-up-items-page/sign-up-items-page.component';
 
 
 @NgModule({
   declarations: [
-    AuthComponent
+    AuthComponent,
+    LoginPageComponent,
+    LoginItemsPageComponent,
+    SignUpItemsPageComponent
   ],
   imports: [
     CommonModule,
