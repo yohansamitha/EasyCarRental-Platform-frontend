16e072ec7d2a38d8c463183865eaa4aa3a07f00e
YohanSamitha <yohansamitha123@gmail.com>
2021-06-19 23:22:32 +0530

    Added user services

--- src/app/modules/auth/services/user.service.ts
+++ src/app/modules/auth/services/user.service.ts
@@ -1,10 +1,36 @@
 import {Injectable} from '@angular/core';
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../../environments/environment";
+import CustomerDTO from "../dto/CustomerDTO";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseURL = environment.baseUrl;
 
-  constructor() {
+  constructor(private http: HttpClient) {
+  }
+
+  public register(dto: CustomerDTO): Observable<any> {
+    return this.http.post(this.baseURL + '/customer', {
+      customerNIC: dto.customerNIC,
+      user_Id: dto.user_Id,
+      firstName: dto.firstName,
+      lastName: dto.lastName,
+      address: dto.address,
+      contact: dto.contact,
+      email: dto.email,
+      licenceNumber: dto.licenceNumber,
+      licenseImagePath: dto.licenseImagePath,
+      nicImagePath: dto.nicImagePath,
+      userDTO: {
+        user_Id: dto.userDTO.user_Id,
+        email: dto.userDTO.email,
+        password: dto.userDTO.password,
+        post: dto.userDTO.post
+      }
+    });
   }
 }
